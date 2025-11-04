673e767c2d1f698ed3116989a03e35e33aad788d
takato0718 <takato.takashi.0718@outlook.jp>
2025-11-04 16:20:20 +0900

    仮のトップページ作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,8 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  private
+  def not_authenticated
+    redirect_to new_user_session_path, alert: "ログインが必要です"
+  end
 end
