58338b4e64a9f0fa772494a26e8bc6279153717a
takato0718 <takato.takashi.0718@outlook.jp>
2025-11-05 11:40:06 +0900

    userモデル作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,14 @@
 class User < ApplicationRecord
+  authenticates_with_sorcery! do |config|
+    config.submodules = [:remember_me, :reset_password]
+  end
+      
+  validates :name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :password, length: { minimum: 6 }, if: -> { new_record? || changes[:crypted_password] }
+  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
+  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
+      
+  has_many :posts, dependent: :destroy
+  has_many :recommendations, dependent: :destroy
 end
