7a0c6b9b80a29dc8bddf7d6a5eec4691b04ca746
takato0718 <takato.takashi.0718@outlook.jp>
2025-11-25 23:25:28 +0900

    本番環境でのソースマップを無効化

--- config/initializers/dartsass.rb
+++ config/initializers/dartsass.rb
@@ -5,3 +5,8 @@ Rails.application.config.dartsass.builds = {
 Rails.application.config.dartsass.load_paths = [
   Rails.root.join("node_modules")
 ]
+
+# 本番環境でのソースマップを無効化
+if Rails.env.production?
+  Rails.application.config.dartsass.build_options = ["--no-source-map"]
+end
