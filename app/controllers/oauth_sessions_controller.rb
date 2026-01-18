dbd3a9d8f6ee1521332c7b4ba979ffde0a03845d
takato0718 <takato.takashi.0718@outlook.jp>
2026-01-18 16:47:01 +0900

    コントローラー

---
+++ app/controllers/oauth_sessions_controller.rb
@@ -0,0 +1,21 @@
+class OauthSessionsController < ApplicationController
+  skip_before_action :require_login, only: [:create]
+
+  def create
+    user_info = request.env['omniauth.auth']
+
+    # ユーザーを検索または作成
+    user = User.find_or_create_from_auth(user_info)
+
+    if user&.persisted?
+      # Sorceryのauto_loginメソッドでログイン
+      auto_login(user)
+      redirect_to root_path, notice: 'Googleアカウントでログインしました'
+    else
+      redirect_to login_path, alert: 'ログインに失敗しました'
+    end
+  rescue StandardError => e
+    Rails.logger.error "OAuth認証エラー: #{e.message}"
+    redirect_to login_path, alert: 'ログインに失敗しました'
+  end
+end
