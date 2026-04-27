d902472d46d64513083bab311af2c019050ea4b8
takato0718 <takato.takashi.0718@outlook.jp>
2026-04-27 19:44:52 +0900

    Xシェア時のコメント変更

--- app/helpers/twitter_share_helper.rb
+++ app/helpers/twitter_share_helper.rb
@@ -1,7 +1,8 @@
 module TwitterShareHelper
   # 投稿詳細画面用のシェアURL
   def post_twitter_share_url(post)
-    text = "「#{post.title}」を紹介します！ #推しツケ"
+    user_name = post.user.name
+    text = "#{user_name}の推し、「#{post.title}」を紹介します！ #推しツケ"
     url = post_url(post)
 
     "https://twitter.com/intent/tweet?text=#{CGI.escape(text)}&url=#{CGI.escape(url)}"
@@ -12,7 +13,7 @@ module TwitterShareHelper
     post = recommendation.post
     recommender = post.user
 
-    text = "#{recommender.name}さんから「#{post.title}」を推しツケられました！✨ #推しツケ"
+    text = "#{recommender.name}さんから「#{post.title}」を推しツケられました！ #推しツケ"
     url = post_url(post)
 
     "https://twitter.com/intent/tweet?text=#{CGI.escape(text)}&url=#{CGI.escape(url)}"
